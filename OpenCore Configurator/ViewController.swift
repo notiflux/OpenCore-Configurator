9418023059cc41742dd64ff147675df5e0a33095
Pavo <brockbackups@gmail.com>
2019-06-09 08:40:24 -0700

    Moved ACPI Add IBActions to new view controller

--- OpenCore Configurator/ViewController.swift
+++ OpenCore Configurator/ViewController.swift
@@ -1272,13 +1272,6 @@ class ViewController: NSViewController {
     @IBAction func timeoutTextfieldAction(_ sender: NSTextField) {
         timeoutStepper.stringValue = sender.stringValue
     }
-    
-    @IBAction func addAcpiBtn(_ sender: Any) {
-        addEntryToTable(table: &acpiAddTable, appendix: ["Path": "", "Comment": "", "Enabled": ""])
-    }
-    @IBAction func remAcpiBtn(_ sender: Any) {
-        removeEntryFromTable(table: &acpiAddTable)
-    }
     @IBAction func blockAcpiBtn(_ sender: Any) {
         addEntryToTable(table: &acpiBlockTable, appendix: ["Comment": "", "OemTableId": "", "TableLength": "", "TableSignature": "DSDT","Enabled": "", "All": ""])
     }
@@ -1387,43 +1380,6 @@ class ViewController: NSViewController {
     
     func addBytes(current: UInt8) throws {
         checksum &+= current
-    }
-    
-    @IBAction func autoAddAcpi(_ sender: Any) {
-        if mountedESP != "" {
-            let fileManager = FileManager.default
-            let acpiUrl = URL(fileURLWithPath: "\(mountedESP)/EFI/OC/ACPI/Custom")
-            do {
-                let fileURLs = try fileManager.contentsOfDirectory(at: acpiUrl, includingPropertiesForKeys: nil)
-                var filenames: [String] = [String]()
-                for i in fileURLs {
-                    if !i.lastPathComponent.hasSuffix(".aml") {
-                        messageBox(message: "\(i.lastPathComponent) does not have the .aml extension.")
-                        continue
-                    }
-                    
-                    let checksum = calcAcpiChecksum(table: i)
-                    if checksum != 0 {
-                        if checksum != nil {
-                            messageBox(message: "Invalid Checksum", info: "The checksum for \(i.lastPathComponent) is invalid.")
-                        } else {
-                            messageBox(message: "The length of \(i.lastPathComponent) could not be verified.")
-                        }
-                        continue
-                    }
-                    filenames.append(i.lastPathComponent)
-                }
-                
-                for file in filenames {
-                    tableLookup[acpiAddTable]!.append(["Comment": "", "Path": file, "Enabled": "1"])
-                }
-                acpiAddTable.reloadData()
-            } catch {
-                print("Error while enumerating files \(acpiUrl.path): \(error.localizedDescription)")
-            }
-        } else {
-            espWarning()
-        }
     }
     
     @IBAction func autoAddKernel(_ sender: Any) {
--- OpenCore Configurator/ViewControllers/ACPI/AcpiAddViewContoller.swift
+++ OpenCore Configurator/ViewControllers/ACPI/AcpiAddViewContoller.swift
@@ -15,4 +15,48 @@ class AcpiAddViewContoller: NSViewController {
         // Do view setup here.
     }
     
+    @IBAction func addAcpiBtn(_ sender: Any) {
+        addEntryToTable(table: &acpiAddTable, appendix: ["Path": "", "Comment": "", "Enabled": ""])
+    }
+    
+    @IBAction func remAcpiBtn(_ sender: Any) {
+        removeEntryFromTable(table: &acpiAddTable)
+    }
+    
+    @IBAction func autoAddAcpi(_ sender: Any) {
+        if mountedESP != "" {
+            let fileManager = FileManager.default
+            let acpiUrl = URL(fileURLWithPath: "\(mountedESP)/EFI/OC/ACPI/Custom")
+            do {
+                let fileURLs = try fileManager.contentsOfDirectory(at: acpiUrl, includingPropertiesForKeys: nil)
+                var filenames: [String] = [String]()
+                for i in fileURLs {
+                    if !i.lastPathComponent.hasSuffix(".aml") {
+                        messageBox(message: "\(i.lastPathComponent) does not have the .aml extension.")
+                        continue
+                    }
+                    
+                    let checksum = calcAcpiChecksum(table: i)
+                    if checksum != 0 {
+                        if checksum != nil {
+                            messageBox(message: "Invalid Checksum", info: "The checksum for \(i.lastPathComponent) is invalid.")
+                        } else {
+                            messageBox(message: "The length of \(i.lastPathComponent) could not be verified.")
+                        }
+                        continue
+                    }
+                    filenames.append(i.lastPathComponent)
+                }
+                
+                for file in filenames {
+                    tableLookup[acpiAddTable]!.append(["Comment": "", "Path": file, "Enabled": "1"])
+                }
+                acpiAddTable.reloadData()
+            } catch {
+                print("Error while enumerating files \(acpiUrl.path): \(error.localizedDescription)")
+            }
+        } else {
+            espWarning()
+        }
+    }
 }
